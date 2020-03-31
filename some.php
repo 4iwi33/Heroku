28f9348ee9c773cf95cb5ea11d1b1fbd76245d96
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-31 21:01:27 +0300

    add

--- some.php
+++ some.php
@@ -1,4 +1,18 @@
 <?php
-for ($i=1; $i <=10 ; $i++) { 
-} echo $i;
+for ($i = 1; $i <= 10; $i++) {
+}
+echo $i;
+
+
+
+/*Username: y8066sYP18
+
+Database name: y8066sYP18
+
+Password: babUFztZwq
+
+Server: remotemysql.com
+
+Port: 3306*/
+
 ?>
